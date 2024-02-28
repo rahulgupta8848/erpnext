7b8c0f1c5433ee51a2a14a851a8369e7e4460cc0
ruthra kumar <ruthra@erpnext.com>
2024-02-28 17:07:37 +0530

    fix: don't override reference exchange rate

--- erpnext/accounts/doctype/payment_entry/payment_entry.py
+++ erpnext/accounts/doctype/payment_entry/payment_entry.py
@@ -485,7 +485,7 @@ class PaymentEntry(AccountsController):
 		self,
 		force: bool = False,
 		update_ref_details_only_for: list | None = None,
-		reference_exchange_details: dict | None = None,
+		ref_exchange_rate: float | None = None,
 	) -> None:
 		for d in self.get("references"):
 			if not d.allocated_amount:
@@ -496,21 +496,11 @@ class PaymentEntry(AccountsController):
 			):
 				continue
 
-			ref_details = get_reference_details(
-				d.reference_doctype,
-				d.reference_name,
-				self.party_account_currency,
-				self.party_type,
-				self.party,
-			)
-
-			# Only update exchange rate when the reference is Journal Entry
-			if (
-				reference_exchange_details
-				and d.reference_doctype == reference_exchange_details.reference_doctype
-				and d.reference_name == reference_exchange_details.reference_name
-			):
-				ref_details.update({"exchange_rate": reference_exchange_details.exchange_rate})
+				ref_details = get_reference_details(
+					d.reference_doctype, d.reference_name, self.party_account_currency
+				)
+				if ref_exchange_rate:
+					ref_details.update({"exchange_rate": ref_exchange_rate})
 
 			for field, value in ref_details.items():
 				if d.exchange_gain_loss:
