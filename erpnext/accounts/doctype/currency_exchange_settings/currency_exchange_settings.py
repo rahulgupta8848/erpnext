852c68fad5d7012cc75b51a8c1bf0898519864c5
ruthra kumar <ruthra@erpnext.com>
2024-03-18 08:22:28 +0530

    refactor: toggle between 'http' and 'https' on exchange rate API

--- erpnext/accounts/doctype/currency_exchange_settings/currency_exchange_settings.py
+++ erpnext/accounts/doctype/currency_exchange_settings/currency_exchange_settings.py
@@ -14,7 +14,7 @@ class CurrencyExchangeSettings(Document):
 
 	from typing import TYPE_CHECKING
 
-	if TYPE_CHECKING:  # pragma: no cover
+	if TYPE_CHECKING:
 		from frappe.types import DF
 
 		from erpnext.accounts.doctype.currency_exchange_settings_details.currency_exchange_settings_details import (
@@ -43,6 +43,7 @@ class CurrencyExchangeSettings(Document):
 
 	def set_parameters_and_result(self):
 		if self.service_provider == "exchangerate.host":
+
 			if not self.access_key:
 				frappe.throw(
 					_("Access Key is required for Service Provider: {0}").format(
@@ -77,7 +78,9 @@ class CurrencyExchangeSettings(Document):
 				transaction_date=nowdate(), to_currency="INR", from_currency="USD"
 			)
 
-		api_url = self.api_endpoint.format(transaction_date=nowdate(), to_currency="INR", from_currency="USD")
+		api_url = self.api_endpoint.format(
+			transaction_date=nowdate(), to_currency="INR", from_currency="USD"
+		)
 
 		try:
 			response = requests.get(api_url, params=params)
@@ -97,19 +100,19 @@ class CurrencyExchangeSettings(Document):
 				]
 		except Exception:
 			frappe.throw(_("Invalid result key. Response:") + " " + response.text)
-		if not isinstance(value, int | float):
+		if not isinstance(value, (int, float)):
 			frappe.throw(_("Returned exchange rate is neither integer not float."))
 
 		self.url = response.url
 
 
 @frappe.whitelist()
-def get_api_endpoint(service_provider: str | None = None, use_http: bool = False):
+def get_api_endpoint(service_provider: str = None, use_http: bool = False):
 	if service_provider and service_provider in ["exchangerate.host", "frankfurter.app"]:
 		if service_provider == "exchangerate.host":
 			api = "api.exchangerate.host/convert"
 		elif service_provider == "frankfurter.app":
-			api = "api.frankfurter.app/{transaction_date}"
+			api = "frankfurter.app/{transaction_date}"
 
 		protocol = "https://"
 		if use_http:
