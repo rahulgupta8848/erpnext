e91d7e7a914e47d20ac9931538bd64df53b2ddcd
Manoj <manoj@8848digital.com>
2025-09-10 16:09:26 +0530

    fix: set draft QC in purchase document on creation of qc

--- erpnext/stock/doctype/quality_inspection/quality_inspection.py
+++ erpnext/stock/doctype/quality_inspection/quality_inspection.py
@@ -192,11 +192,10 @@ class QualityInspection(Document):
 		self.get_item_specification_details()
 
 	def on_update(self):
-		action_if_qi_in_draft = frappe.db.get_single_value(
-			"Stock Settings", "action_if_quality_inspection_is_not_submitted"
-		)
-
-		if not action_if_qi_in_draft or action_if_qi_in_draft == "Warn":
+		if (
+			frappe.db.get_single_value("Stock Settings", "action_if_quality_inspection_is_not_submitted")
+			== "Warn"
+		):
 			self.update_qc_reference()
 
 	def on_submit(self):
