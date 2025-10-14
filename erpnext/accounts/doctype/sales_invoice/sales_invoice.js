4c897b1ad34d383e3947c36dce6b2f73f3df72fc
Khushi Rawat <142375893+khushi8112@users.noreply.github.com>
2025-10-14 17:25:48 +0530

    fix: resolve conflict

--- erpnext/accounts/doctype/sales_invoice/sales_invoice.js
+++ erpnext/accounts/doctype/sales_invoice/sales_invoice.js
@@ -752,10 +752,7 @@ frappe.ui.form.on("Sales Invoice", {
 					is_group: 0,
 				},
 			};
-<<<<<<< HEAD
 		};
-=======
-		});
 
 		frm.set_query("sales_person", "sales_team", function () {
 			return {
@@ -765,7 +762,6 @@ frappe.ui.form.on("Sales Invoice", {
 				},
 			};
 		});
->>>>>>> 2fcd406b18 (fix: filter sales team to show only active individual salespersons)
 	},
 
 	onload: function (frm) {
