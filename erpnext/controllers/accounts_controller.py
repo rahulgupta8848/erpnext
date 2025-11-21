4b098993fb8d135653f6f6fe2b96b7f0a4661ef3
ashish <ashish.verma@8848digital.com>
2025-11-21 14:45:01 +0600

    fix: DeliveryNoteItem object has no attribute against_pick_list

--- erpnext/controllers/accounts_controller.py
+++ erpnext/controllers/accounts_controller.py
@@ -1142,13 +1142,13 @@ class AccountsController(TransactionBase):
 		if self.get("taxes") or self.get("is_pos"):
 			return
 
-		if frappe.get_single_value(
+		if frappe.db.get_single_value(
 			"Accounts Settings", "add_taxes_from_taxes_and_charges_template"
 		) and hasattr(self, "taxes_and_charges"):
 			if tax_master_doctype := self.meta.get_field("taxes_and_charges").options:
 				self.append_taxes_from_master(tax_master_doctype)
 
-		if frappe.get_single_value("Accounts Settings", "add_taxes_from_item_tax_template"):
+		if frappe.db.get_single_value("Accounts Settings", "add_taxes_from_item_tax_template"):
 			self.append_taxes_from_item_tax_template()
 
 	def append_taxes_from_master(self, tax_master_doctype=None):
